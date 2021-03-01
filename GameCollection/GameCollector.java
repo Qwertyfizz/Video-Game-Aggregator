f970d89f0eba02c0ddbf037a7c2b256378b0f6e5
Dylan <Dylan.bollman.19@cnu.edu>
2021-03-01 11:29:27 -0500

    added new search method in GameCollector

--- GameCollection/GameCollector.java
+++ GameCollection/GameCollector.java
@@ -1,6 +1,46 @@
 import java.util.ArrayList;
 
-public abstract class GameCollector
+public class GameCollector
 {
-	private ArrayList<Game> gameList;
+    private ArrayList<Game> gameList;
+
+    public
+        void
+        scan(ArrayList<String> filepaths, char scanType, PlatformName platform)
+    {
+
+    }
+
+
+    public ArrayList<Game> getGameList()
+    {
+        return gameList;
+    }
+
+
+    public Game getGameByName(String name)
+    {
+        for (Game g : gameList)
+        {
+            if (g.getName().equals(name))
+            {
+                return g;
+            }
+        }
+        return null;
+    }
+
+
+    public ArrayList<Game> getGamesByPlatform(PlatformName platform)
+    {
+        ArrayList<Game> platList = new ArrayList<>();
+        for (Game g : gameList)
+        {
+            if (g.getPlatform() == platform)
+            {
+                platList.add(g);
+            }
+        }
+        return platList;
+    }
 }
--- src/InputOutput.java
+++ src/InputOutput.java
@@ -1,24 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class InputOutput
 {
     public ArrayList<String> readFile(String filename)
     {
-        try
+        File file = new File(filename);
+        try (Scanner reader = new Scanner(file))
         {
-            File file = new File(filename);
-            Scanner reader = new Scanner(file);
             ArrayList<String> list = new ArrayList<>();
             while (reader.hasNextLine())
             {
                 list.add(reader.nextLine());
             }
-            reader.close();
             return list;
         }
         catch (FileNotFoundException e)
@@ -31,29 +29,38 @@ public class InputOutput
 
     public void writeFile(String filename, ArrayList<String> list)
     {
-    	
-			try (FileWriter writer = new FileWriter(filename)) {
-				for (String s : list) {
-					writer.write(s);
-				}
-			} catch (IOException e1) {
-				System.out.println("ERROR:");
-				e1.printStackTrace();
-			}
-		
+
+        try (FileWriter writer = new FileWriter(filename))
+        {
+            for (String s : list)
+            {
+                writer.write(s);
+            }
+        }
+        catch (IOException e1)
+        {
+            System.out.println("ERROR:");
+            e1.printStackTrace();
+        }
+
     }
-    
-    
-    public void createFile(String filepath) {
-    	try {
-			File newFile = new File(filepath);
-			if (newFile.createNewFile()) {
-				
-			}
-		} catch (IOException e) {
-			System.out.println("ERROR:");
-			e.printStackTrace();
-		}
+
+
+    public void createFile(String filepath)
+    {
+        try
+        {
+            File newFile = new File(filepath);
+            if (newFile.createNewFile())
+            {
+
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("ERROR:");
+            e.printStackTrace();
+        }
     }
 
 }
