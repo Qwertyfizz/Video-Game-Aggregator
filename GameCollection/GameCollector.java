c69c0de385dde6863746e578fc1208bb5a5f6462
Qwertyfizz <60331360+Qwertyfizz@users.noreply.github.com>
2021-03-02 16:26:15 -0500

    started implementation of file search algorithm

--- GameCollection/GameCollector.java
+++ GameCollection/GameCollector.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 
 public class GameCollector
@@ -6,7 +7,37 @@ public class GameCollector
 
     public void scan(ArrayList<String> filepaths, char scanType, PlatformName platform)
     {
-    	
+    	for (String filepath : filepaths) {
+    		File currFilepath = new File(filepath);
+    		File[] dirList = currFilepath.listFiles();
+    		for(File dir : dirList) {
+    			String tempName = dir.getName();
+    			String tempPath = dir.getAbsolutePath();
+    			String tempExe = findExe(dir).getAbsolutePath(); //TODO: Implement Exe search algo (See design doc)
+    			
+    			gameList.add(new Game(tempName, tempPath, tempExe, platform));
+    		}
+    	}
+    }
+    
+    public File findExe(File directory) {
+    	File[] dirList = directory.listFiles();
+    	ArrayList<File> exeList = new ArrayList<>();
+    	for (File f : dirList) {
+    		if (f.getName().contains(".exe") && !f.isDirectory()) {
+    			exeList.add(f);
+    		}
+    	}
+    	if (exeList.size() == 1) {
+    		return exeList.get(0);
+    	}
+    	else if(exeList.size() >= 2) {
+    		//TODO: Implement file diff
+    	}
+    	else {
+    		//TODO: Implement recursive call to search deeper file
+    	}
+		return directory;
     }
 
     public void addGame(Game g) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
 
+	//TODO: Replace test code with CIL driver
 	public static void main(String[] args) {
 		
 		Game game = new Game("Cod", "Path", "exe.exe", PlatformName.ORIGIN);
