ba331cf6ed73de716c9610dfdcbc43e45e75ca9a
Dylan <Dylan.bollman.19@cnu.edu>
2021-03-02 18:15:44 -0500

    started implementation of http drivers

--- GameCollection/Steam.java
+++ GameCollection/Steam.java
@@ -1,7 +1,38 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Steam extends GameCollector {
-	private String[] directories = {"C:\\Steam\\steamapps\\common", ".\\steamapps\\common"};
-	
-	
+	private String[] directories = { "C:\\Steam\\steamapps\\common", ".\\steamapps\\common" };
+	private String key = "";
+
+	public void getOwnedGames() {
+		URL url;
+		try {
+			url = new URL(
+					"https://partner.steam-api.com/ISteamUser/CheckAppOwnership/v2/?key=**********&steamid=76561198050400292&appid=659540");
+
+			try {
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+				String inputLine;
+
+				while ((inputLine = in.readLine()) != null) {
+					System.out.println(inputLine);
+				}
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
