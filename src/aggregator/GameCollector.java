9b308380963b034ae5cb081e0218448b53998efe
Qwertyfizz <60331360+Qwertyfizz@users.noreply.github.com>
2021-04-22 10:21:55 -0400

    Removed deprecated exe finder

--- src/aggregator/GameCollector.java
+++ src/aggregator/GameCollector.java
@@ -28,60 +28,6 @@ public abstract class GameCollector {
 	
 	public abstract boolean checkForPlatform(); // Function to check for platform install
 
-	public File findExe(File directory) {
-		System.out.println("Working on " + directory.getName());
-		String[] extention = { "exe" };
-		ArrayList<File> exeList = new ArrayList<>(directorySearch(directory, extention, false));
-
-		if (exeList.size() == 1) {// If it's the only file in the first directory, It is likely the main exe
-			return exeList.get(0);
-		} else if (exeList.size() >= 2) // Strip out common executables and look again
-		{
-			for (int i = 0; i < exeList.size(); i++) {
-				for (int j = 0; j < nonGameLaunchers.length; j++) {
-					if (exeList.get(i).getName().contains(nonGameLaunchers[j])) {
-						exeList.remove(i);
-						break;
-					}
-				}
-			}
-			if (exeList.size() == 1) {
-				return exeList.get(0);
-			}
-			for (File exe : exeList) {
-				if (exe.getName().toLowerCase().contains("launch")) {
-					return exe;
-				}
-			}
-		}
-		// Get a list of ALL executables in the directories
-		// Strip out all known non game launchers
-		// look for good hints at which is the correct one (name of game in file(No
-		// spaces, Spaces to _), first letters only, etc.)
-		// WILL BE SLOW - AVOID IF POSSIBLE
-		else {
-			String name = directory.getName();
-			exeList.clear();
-			exeList = new ArrayList<>(directorySearch(directory, extention, true));
-			for (int i = 0; i < exeList.size(); i++) {
-				for (int j = 0; j < nonGameLaunchers.length; j++) {
-					if (exeList.get(i).getName().contains(nonGameLaunchers[j])) {
-						exeList.remove(i);
-						break;
-					}
-				}
-			}
-			if (exeList.size() == 1) {
-				return exeList.get(0);
-			} else if (!exeList.isEmpty()) {
-				return new File(" >= 2");
-			}
-
-		}
-		// Nothing in there! Indicates a left over file from a previously installed game
-		return new File("No exes are present");
-	}
-
 	public Collection<File> directorySearch(File dir, String[] extentions, boolean searchAll) {
 
 		return FileUtils.listFiles(dir, extentions, searchAll);
